30fc4b47587d8f94dfc7137b06c5726937661cae
ElizPN <pankevych.liza@gmail.com>
2023-07-01 13:20:52 +0200

    create customers component

---
+++ src/features/customers/Customers.js
@@ -0,0 +1,26 @@
+import { useSelector, useDispatch } from "react-redux";
+import { addCustomer } from "./customersSlice";
+
+export function Customers() {
+  const customers = useSelector((state) => state.customers.customers);
+  const dispatch = useDispatch();
+  console.log(customers);
+
+  return (
+    <div>
+      <button onClick={() => dispatch(addCustomer(prompt()))}>
+        Add customer
+      </button>
+
+      {customers?.length > 0 ? (
+        <div>
+          {customers.map((customer) => (
+            <p>{customer}</p>
+          ))}
+        </div>
+      ) : (
+        <div>No customers</div>
+      )}
+    </div>
+  );
+}
