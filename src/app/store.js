de8fdd97d546149682d940d327ab928851ba98d0
ElizPN <pankevych.liza@gmail.com>
2023-07-01 13:19:21 +0200

    add customerReduser to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-//  counterSlice.reducer
+//  counterSlice.reducer, customersSlice.reducer default ecport
 import counterReducer from "../features/counter/counterSlice";
+import customerReduser from "../features/customers/customersSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    customers: customerReduser,
   },
 });
